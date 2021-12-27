0a27716dc8cdf636a4322fb717e51898b1391688
ZainabDaebes <62718985+ZainabDaebes@users.noreply.github.com>
2021-12-27 11:32:27 +0100

    create project

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Main.main");
+        System.out.println("Main.main 23-post-person");
+        System.out.println("Main.main 3");
+
+        Person person = new Person("Arne", 100);
+
+        System.out.println("Main.main "+person.getName());
+    }
+}
